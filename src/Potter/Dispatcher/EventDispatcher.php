9bbf9e5dc26b021f5e7365d6ab2153016404a6e9
Jay Potter <j@ypotter.ca>
2024-03-30 18:53:39 -0400

    Update EventDispatcher.php

--- src/Potter/Dispatcher/EventDispatcher.php
+++ src/Potter/Dispatcher/EventDispatcher.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Potter\Dispatcher;
 
+use \Psr\EventDispatcher\ListenerProviderInterface;
+
 final class EventDispatcher extends AbstractEventDispatcher
 {
-
+    use EventDispatcherTrait;
+    
+    public function __construct(ListenerProviderInterface $listenerProvider)
+    {
+        $this->setListenerProvider($listenerProvider);
+    }
 }
